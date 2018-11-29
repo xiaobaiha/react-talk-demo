b849045d733a883122a4855c867075fa60c86a5f
feizhijun@momenta <feizhijun@momenta.ai>
2018-11-29 14:58:33 +0800

    feat: codemirror

---
+++ src/demos/part1/ClassTemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Welcome extends React.Component {
+    render() {
+      return <h1>Hello, class!</h1>;
+    }
+}
+
+export const Template = 
+`class WelcomeClass extends React.Component {
+    render() {
+      return <h1>Hello, class!</h1>;
+    }
+}
+`;
